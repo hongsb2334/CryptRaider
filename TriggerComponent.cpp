10ecbf84fa595714a91a6df3da4c042762409ad6
hongsb2334 <119171890+hongsb2334@users.noreply.github.com>
2025-02-06 22:15:36 +0900

    ~#101 TArray

--- TriggerComponent.cpp
+++ TriggerComponent.cpp
@@ -23,4 +23,12 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+    TArray<AActor*> Actors;                                             //AActor 타입의 TArray(배열 컨테이너) 생성 후 GetOverlappingActors 호출해서 오버래핑된 컴포넌트를 리턴
+    GetOverlappingActors(Actors);
+
+    if(Actors.Num() > 0)                                                //Actors 원소가 0보다 클때(오버래핑된 경우) 오버래핑된 액터 이름 출력
+    {
+        FString ActorName = Actors[0]->GetActorNameOrLabel();
+        UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+    }
 }
